70f19f4767f0a6490deaae42df524b64828546d0
Ali <adadashipour@gmail.com>
2024-09-26 12:17:14 +0100

    Insert separate wage calculating method

---
+++ EmployeeManager/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    class Employee
+    {
+        public string employeeName { get; set; }
+        public string employeeID { get; set; }
+        public double hoursWorked { get; set; }
+        public double hourlyRate = 9.5;
+
+        public Employee(string employeeName, string employeeID, double hoursWorked)
+        {
+            this.employeeName = employeeName;
+            this.employeeID = employeeID;
+            this.hoursWorked = hoursWorked;//in a week
+        }
+
+        public double getWage()
+        {
+            return hoursWorked * hourlyRate;
+        }
+
+        public override string ToString()
+        {
+            double wage = getWage();
+            return $"Name: {employeeName} \nEmployee ID: {employeeID} \nHours Worked: {hoursWorked} \n\nThe weekly wage is £{wage:F2}";
+        }
+    }
+
--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -1,27 +1,7 @@
 ﻿using System;
 
-namespace EmployeeManager
+namespace EmployeeManager 
 {
-    class Employee
-    {
-        public string employeeName { get; set; }
-        public string employeeID { get; set; }
-        public double hoursWorked { get; set; }
-        public double hourlyRate = 9.5;
-
-        public Employee(string employeeName, string employeeID, double hoursWorked)
-        {
-            this.employeeName = employeeName;
-            this.employeeID = employeeID;
-            this.hoursWorked = hoursWorked;//in a week
-        }
-
-        public override string ToString()
-        {
-            return $"Name: {employeeName} \nEmployee ID: {employeeID} \nHours Worked: {hoursWorked} \n\nThe weekly wage is £{hoursWorked * hourlyRate}";
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
